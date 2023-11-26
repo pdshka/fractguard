65f2b8a27a292f94d87888aeeee980702632989d
comprock15 <khoruzhaia@sfedu.ru>
2023-11-26 22:28:38 +0300

    Add text appears from darkness

--- Assets/Scripts/EndingController.cs
+++ Assets/Scripts/EndingController.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 public class EndingController : MonoBehaviour
 {
     [SerializeField] private Image background;
+    [SerializeField] private Image blackScreen;
     [SerializeField] private Sprite sprite;
+
     private int possibility = 5;
 
     private void Start()
@@ -17,5 +19,16 @@ public class EndingController : MonoBehaviour
             background.color = Color.white;
             background.sprite = sprite;
         }
+        StartCoroutine(LightenBlackScreen());
+    }
+
+    private IEnumerator LightenBlackScreen()
+    {
+        while (blackScreen.color.a > 0)
+        {
+            yield return new WaitForSeconds(0.1f);
+            blackScreen.color = new Color(blackScreen.color.r, blackScreen.color.g, blackScreen.color.b, blackScreen.color.a - 0.05f);
+        }
+        blackScreen.gameObject.SetActive(false);
     }
 }
